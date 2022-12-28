a812c3cd884d3095293b06be0a20bc9ac5da5752
Buse Özbaytekin <buseozbaytekin@hotmail.com>
2022-12-28 17:18:45 +0300

    create users with functional method

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,8 +1,12 @@
 package com.example.backend;
 
+import com.example.backend.dto.UserCreateDTO;
+import com.example.backend.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
@@ -12,4 +16,26 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService){
+		return (args) -> {
+			UserCreateDTO user = new UserCreateDTO();
+			user.setUserName("user1");
+			user.setFirstName("Buse");
+			user.setLastName("Ozbaytekin");
+			userService.createUser(user);
+
+			UserCreateDTO user2 = new UserCreateDTO();
+			user2.setUserName("user2");
+			user2.setFirstName("Mustafa");
+			user2.setLastName("Bilce");
+			userService.createUser(user2);
+
+			UserCreateDTO user3 = new UserCreateDTO();
+			user3.setUserName("user3");
+			user3.setFirstName("Buse");
+			user3.setLastName("Bilce");
+			userService.createUser(user3);
+		};
+	}
 }
